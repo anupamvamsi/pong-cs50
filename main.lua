11729bb06eb752231f369ead3c10db6c4c49d5b2
anupamvamsi <anupam.ismav@gmail.com>
2022-07-09 23:51:30 +0530

    1. Added push as a requirement, 2. Added virtual dimensions of WxH, 3. Included filter back in love.load(), 4. setupScreen() for virtual dimensions, 5. Implemented love.resize(w, h) using push, 6. Updated love.draw() with background color (maroon), centered 'PONG' text

--- main.lua
+++ main.lua
@@ -1,21 +1,37 @@
+-- https://github.com/Ulydev/push
+push = require('push')
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
   fontLarge = love.graphics.setNewFont('font.ttf', 48)
-  love.graphics.setBackgroundColor(123 / 255, 21 / 255, 14 / 255, 255)
   love.window.setTitle('Pong!')
-  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     resizable = true,
     vsync = true
   })
 end
 
+function love.resize(w, h)
+  push:resize(w, h)
+end
+
 function love.draw()
-  love.graphics.setDefaultFilter("nearest", "nearest")
+  push:apply('start')
+
+  love.graphics.clear(123 / 255, 21 / 255, 14 / 255, 255)
   love.graphics.setFont(fontLarge)
-  love.graphics.print('PONG', WINDOW_WIDTH / 2 - 24, WINDOW_HEIGHT / 2 - 24)
+  love.graphics.print('PONG', VIRTUAL_WIDTH / 2 - 48, VIRTUAL_HEIGHT / 2 - 24)
+
+  push:apply('end')
 end
 
 function love.keypressed(key)
