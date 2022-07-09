e40d5b11fd81fe588245f0b34bcd7f9a2e0b380c
anupamvamsi <anupam.ismav@gmail.com>
2022-07-10 00:06:56 +0530

    1. Changed large font size to 24, 2. Added paddles in love.draw()

--- main.lua
+++ main.lua
@@ -7,10 +7,12 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+FONT_LARGE = 24
+
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
 
-  fontLarge = love.graphics.setNewFont('font.ttf', 48)
+  fontLarge = love.graphics.setNewFont('font.ttf', FONT_LARGE)
   love.window.setTitle('Pong!')
 
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -29,7 +31,10 @@ function love.draw()
 
   love.graphics.clear(123 / 255, 21 / 255, 14 / 255, 255)
   love.graphics.setFont(fontLarge)
-  love.graphics.print('PONG', VIRTUAL_WIDTH / 2 - 48, VIRTUAL_HEIGHT / 2 - 24)
+  love.graphics.print('PONG', VIRTUAL_WIDTH / 2 - FONT_LARGE, VIRTUAL_HEIGHT / 4 - (FONT_LARGE / 2))
+
+  love.graphics.rectangle("fill", 5, 5, 5, 20)
+  love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 25, 5, 20)
 
   push:apply('end')
 end
